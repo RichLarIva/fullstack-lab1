a24713252b9c0e78f38130256fd9e940a8912197
Richard <54506990+RichLarIva@users.noreply.github.com>
2025-04-13 02:12:33 +0200

    Update Dish.ts

--- src/models/Dish.ts
+++ src/models/Dish.ts
@@ -11,3 +11,19 @@ export interface IDish extends Document
     spiceLevel: string; // A custom field to make it somewhat more original
 }
 
+// Mongoose shcema definition
+const DishSchema: Schema = new Schema(
+    {
+        name: { type: String, required: true, unique: true },
+        ingredients: [String],
+        preparationSteps: [String],
+        cookingTime: String,
+        origin: String,
+        spiceLevel: String,
+    },
+    {timestamps: true} // Automatically adds createdAt and updatedAt
+);
+
+// Exports the mongoose model
+
+export default mongoose.model<IDish>("Dish", DishSchema);
